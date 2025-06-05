fb73690496a6c134d7f594601e8a3fe6d6e9caba
Calle Villca Rubi Ivana <rcallevillca@frba.utn.edu.ar>
2025-06-05 19:21:12 -0300

    terminar ej 1

--- Arrays/signo_zodiacal.cpp
+++ Arrays/signo_zodiacal.cpp
@@ -3,39 +3,16 @@ Escribir funciones que resuelvan los siguientes problemas:
 
 Dado un año, que devuelva si es bisiesto. Nota: un año es bisiesto si es un número divisible por 4, 
 pero no si es divisible por 100, excepto que también sea divisible por 400.
-Dado un mes y un año, que devuelva la cantidad de días correspondientes.
-Pedirle al usuario su día y mes de cumpleaños. El programa debe imprimir un mensaje indicando a qué signo 
+1.Dado un mes y un año, que devuelva la cantidad de días correspondientes.
+2.Pedirle al usuario su día y mes de cumpleaños. El programa debe imprimir un mensaje indicando a qué signo 
 corresponde el usuario.
 armar una matriz
 
 datos:    dia y mes 
 marzo 
-del 1 al 20 es piscis  marzo
-dell 21 all 31 es aries
-        if( strcmp(mes,"marzo"))
-        {
-            if( dia >=1 && dia <=20 )
-                soy piscis
-            if( dia >=21 && dia <=31 )
-                soy aries
-        }
-
-...
-sii mes == mayo o mes = abri l , entonces evaluo tod oabril y mayo
-    antes viene abril 1 al 20 es aries
-    intervalo(dia<1 && dia<=20) del 21 de abril a fin de abril 30
-    es Tauro
-    
-    intervalo(dia>=20 && dia<=31)
-    es geminis 
-    
-
-Tauro: 21 de abril al 20 de mayo.
-Géminis: 21 de mayo al 21 de junio.
-
-
+del 1 al 20 es piscis  
+dell 21 al 31 es aries
 ----
-
 Aries: 21 de marzo al 20 de abril.
 Tauro: 21 de abril al 20 de mayo.
 Géminis: 21 de mayo al 21 de junio.
@@ -48,8 +25,8 @@ Sagitario: 23 de noviembre al 21 de diciembre.
 Capricornio: 22 de diciembre al 20 de enero.
 Acuario: 21 de enero al 19 de febrero.
 Piscis: 20 de febrero al 20 de marzo.
- */
- #include <iostream>
+*/
+#include <iostream>
 #include <string.h>
 using namespace std;
 
